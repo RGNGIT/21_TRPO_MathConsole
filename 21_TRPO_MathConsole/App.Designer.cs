720345678a3c6178bf78669588de3f52136fbb76
RGNGIT <vnifedow@bk.ru>
2023-05-17 18:16:51 +0500

    Forms

---
+++ 21_TRPO_MathConsole/App.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace _21_TRPO_MathConsole
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            chartNormal = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            chartExp = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            dataGridViewNormal = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            dataGridViewExp = new DataGridView();
+            Column2 = new DataGridViewTextBoxColumn();
+            buttonStart = new Button();
+            labelResult = new Label();
+            ((System.ComponentModel.ISupportInitialize)chartNormal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chartExp).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewNormal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewExp).BeginInit();
+            SuspendLayout();
+            // 
+            // chartNormal
+            // 
+            chartArea1.Name = "ChartArea1";
+            chartNormal.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chartNormal.Legends.Add(legend1);
+            chartNormal.Location = new Point(12, 12);
+            chartNormal.Name = "chartNormal";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chartNormal.Series.Add(series1);
+            chartNormal.Size = new Size(576, 426);
+            chartNormal.TabIndex = 0;
+            chartNormal.Text = "chart1";
+            // 
+            // chartExp
+            // 
+            chartArea2.Name = "ChartArea1";
+            chartExp.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            chartExp.Legends.Add(legend2);
+            chartExp.Location = new Point(12, 444);
+            chartExp.Name = "chartExp";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            chartExp.Series.Add(series2);
+            chartExp.Size = new Size(576, 426);
+            chartExp.TabIndex = 1;
+            chartExp.Text = "chart1";
+            // 
+            // dataGridViewNormal
+            // 
+            dataGridViewNormal.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewNormal.Columns.AddRange(new DataGridViewColumn[] { Column1 });
+            dataGridViewNormal.Location = new Point(594, 12);
+            dataGridViewNormal.Name = "dataGridViewNormal";
+            dataGridViewNormal.RowTemplate.Height = 25;
+            dataGridViewNormal.Size = new Size(383, 426);
+            dataGridViewNormal.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Нормальный";
+            Column1.Name = "Column1";
+            Column1.Width = 150;
+            // 
+            // dataGridViewExp
+            // 
+            dataGridViewExp.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewExp.Columns.AddRange(new DataGridViewColumn[] { Column2 });
+            dataGridViewExp.Location = new Point(594, 444);
+            dataGridViewExp.Name = "dataGridViewExp";
+            dataGridViewExp.RowTemplate.Height = 25;
+            dataGridViewExp.Size = new Size(383, 426);
+            dataGridViewExp.TabIndex = 3;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Экспоненциальный";
+            Column2.Name = "Column2";
+            Column2.Width = 150;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(594, 876);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(383, 76);
+            buttonStart.TabIndex = 4;
+            buttonStart.Text = "Старт";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Location = new Point(12, 873);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(0, 15);
+            labelResult.TabIndex = 5;
+            // 
+            // App
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(989, 964);
+            Controls.Add(labelResult);
+            Controls.Add(buttonStart);
+            Controls.Add(dataGridViewExp);
+            Controls.Add(dataGridViewNormal);
+            Controls.Add(chartExp);
+            Controls.Add(chartNormal);
+            Name = "App";
+            Text = "App";
+            ((System.ComponentModel.ISupportInitialize)chartNormal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartExp).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewNormal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewExp).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartNormal;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartExp;
+        private DataGridView dataGridViewNormal;
+        private DataGridView dataGridViewExp;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private Button buttonStart;
+        private Label labelResult;
+    }
+}
---
+++ 21_TRPO_MathConsole/App.cs
@@ -0,0 +1,183 @@
+﻿namespace _21_TRPO_MathConsole
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+            MathNormalHelper.SetupNormalTables();
+            MathExponentialHelper.SetupExpTables();
+            chartNormal.Series.Clear();
+            chartExp.Series.Clear();
+            chartNormal.Series.Add("Нормальный");
+            chartExp.Series.Add("Экспоненциальный");
+        }
+
+        List<double> normals = new List<double>();
+        List<double> exps = new List<double>();
+
+        List<double> normalgram = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        List<double> expgram = new List<double>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        List<double> divgram = new List<double>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        double normalsum = 0;
+        double expsum = 0;
+        double normalavg;
+        double expavg;
+
+        double NormalDiv(double elem)
+        {
+            normalsum += elem;
+            if (elem >= 0 && elem <= 2)
+            {
+                normalgram[0]++;
+            }
+            if (elem > 2 && elem <= 4)
+            {
+                normalgram[1]++;
+            }
+            if (elem > 4 && elem <= 6)
+            {
+                normalgram[2]++;
+            }
+            if (elem > 6 && elem <= 8)
+            {
+                normalgram[3]++;
+            }
+            if (elem > 8 && elem <= 10)
+            {
+                normalgram[4]++;
+            }
+            if (elem > 10 && elem <= 12)
+            {
+                normalgram[5]++;
+            }
+            if (elem > 12 && elem <= 14)
+            {
+                normalgram[6]++;
+            }
+            if (elem > 14 && elem <= 16)
+            {
+                normalgram[7]++;
+            }
+            if (elem > 16 && elem <= 18)
+            {
+                normalgram[8]++;
+            }
+            if (elem > 18 && elem <= 20)
+            {
+                normalgram[9]++;
+            }
+            return elem;
+        }
+
+        double ExpDiv(double elem)
+        {
+            expsum += elem;
+            if (elem >= 0 && elem <= 0.2)
+            {
+                expgram[0]++;
+            }
+            if (elem > 0.2 && elem <= 0.4)
+            {
+                expgram[1]++;
+            }
+            if (elem > 0.4 && elem <= 0.6)
+            {
+                expgram[2]++;
+            }
+            if (elem > 0.6 && elem <= 0.8)
+            {
+                expgram[3]++;
+            }
+            if (elem > 0.8 && elem <= 1)
+            {
+                expgram[4]++;
+            }
+            if (elem > 1 && elem <= 1.2)
+            {
+                expgram[5]++;
+            }
+            if (elem > 1.2 && elem <= 1.4)
+            {
+                expgram[6]++;
+            }
+            if (elem > 1.4 && elem <= 1.6)
+            {
+                expgram[7]++;
+            }
+            if (elem > 1.6 && elem <= 1.8)
+            {
+                expgram[8]++;
+            }
+            if (elem > 1.8 && elem <= 2)
+            {
+                expgram[9]++;
+            }
+            return elem;
+        }
+
+        double NormalDispersion()
+        {
+            for (int i = 0; i < normals.Count; i++)
+            {
+                normals[i] -= normalavg;
+            }
+            for (int i = 0; i < normals.Count; i++)
+            {
+                normals[i] = Math.Pow(normals[i], 2);
+            }
+            double sum = 0;
+            foreach (int i in normals)
+            {
+                sum += i;
+            }
+            return sum / (normals.Count - 1);
+        }
+
+        double ExpDispersion()
+        {
+            for (int i = 0; i < exps.Count; i++)
+            {
+                exps[i] -= expavg;
+            }
+            for (int i = 0; i < exps.Count; i++)
+            {
+                exps[i] = Math.Pow(exps[i], 2);
+            }
+            double sum = 0;
+            foreach (int i in exps)
+            {
+                sum += i;
+            }
+            return sum / (exps.Count - 1);
+        }
+
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Visible = false;
+            for (int i = 0; i < 20; i++)
+            {
+                double res = NormalDiv(MathNormalHelper.Normal(4, 5));
+                normals.Add(res);
+                dataGridViewNormal.Rows.Add(res);
+            }
+            for (int i = 0; i < 80; i++)
+            {
+                double res = ExpDiv(MathExponentialHelper.Exponential(3));
+                exps.Add(res);
+                dataGridViewExp.Rows.Add(res);
+            }
+            chartNormal.Series["Нормальный"].Points.DataBindXY(divgram.ToArray(), normalgram.ToArray());
+            chartExp.Series["Экспоненциальный"].Points.DataBindXY(divgram.ToArray(), expgram.ToArray());
+            normalavg = normalsum / 20;
+            expavg = expsum / 80;
+            labelResult.Text = $"Среднее значение нормального распределения: {normalavg}\n" +
+            $"Среднее значение экспоненциального распределения: {expavg}\n" +
+            $"Дисперсия нормального распределения: {NormalDispersion()}\n" +
+            $"Дисперсия экспоненциального распределения: {ExpDispersion()}";
+        }
+    }
+}
